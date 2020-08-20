1d531b3e8d5a48c727cceaaf16f716b08551dd6e
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2020-08-19 22:20:27 -0400

    Complete task of creating a calculator with React

---
+++ week-11-homework-react-math-calc/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Calculator from './Calculator'
+import './App.css';
+
+
+
+function App() {
+  
+  return (
+    <div className="app calculator">
+      <header>
+      React Calculator
+      </header>
+
+     <Calculator/>
+
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ week-11-homework-react-math-calc/src/Calculator.js
@@ -0,0 +1,113 @@
+import React from 'react';
+
+
+
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+          value1: 0,
+          value2: 0,
+          operator: ["+","-","*"],
+         
+          result: ''
+
+        };
+
+        //"this" only exists for event handlers if we include lines like this for each
+        // event handler
+        //this.onValue1Change.bind(this)
+        this.calculate = this.calculate.bind(this)
+      }
+       
+      /**
+       event target is the element that had the event 
+       -in this case the value1 input had a "change" event
+    
+       */
+      onValue1Change = (event) => {
+        const newValue = event.target.value;
+        this.setState({
+          value1: newValue
+        })
+        debugger
+      }
+      
+      onValue2Change = (event) => {
+        const newValue = event.target.value;
+        this.setState({
+          value2: newValue
+        })
+      }
+
+      onOperatorChange = (event) =>{
+        const newValue = event.target.value;
+        this.setState({
+          operator: newValue
+        })
+      }
+      //
+      calculate = () => {
+        let result = 0;
+        const {value1, value2, operator} =this.state;
+        
+        switch(operator) {
+          case '+':
+            result = parseInt(value1) + parseInt(value2);
+            break;
+          case '-':
+            result = parseInt(value1) - parseInt(value2);
+            break;
+          case '*':
+            result = parseInt(value1) * parseInt(value2);
+            break;
+          case '/':
+            result = parseInt(value1) / parseInt(value2);
+            break;
+          default:
+            result = 0;
+        }
+        this.setState({
+          result
+        })
+      }
+      render() {
+        
+      
+    
+        return (
+          <div>
+             {/* first operand */}
+                <input type= "number" onChange={this.onValue1Change} />
+
+                <select onChange={this.onOperatorChange}>
+                  <option>+</option>
+                  <option>-</option>
+                  <option>*</option>
+                  <option>/</option>
+                </select>
+
+                {/* second operand*/}
+                <input type="number" onChange={this.onValue2Change}/>
+
+                <button onClick={this.calculate}> =</button>
+
+                <input value={this.state.result} type= "text" disabled/>
+
+          </div>
+        
+        );
+       
+       }
+
+        
+      
+    
+}
+
+
+
+
+
+export default Calculator;
